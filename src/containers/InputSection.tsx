08258bda66c79286eda70ed83a80ac31fa865c7c
Morhaf Shamia <inmogr@msn.com>
2025-01-23 20:42:16 +0400

    added parent id

--- src/containers/InputSection.tsx
+++ src/containers/InputSection.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import SingleLineTextInput from '../components/SingleLineTextInput';
 import RadioGroupInput from '../components/RadioGroupInput';
 import CheckBoxGroupInput from '../components/CheckBoxGroupInput';
-import {Controller, useForm, FormProvider} from 'react-hook-form';
-import {FormPropertyType, FormSectionType} from '../types/FormTypes';
+import { Controller, useForm, FormProvider } from 'react-hook-form';
+import { FormPropertyType, FormSectionType } from '../types/FormTypes';
 import TypeAheadTextInput from '../components/TypeAheadTextInput';
 import Collapsible from 'react-native-collapsible';
-import {STORED_FROM_LAST_SESSION, SaveAndSubmitForm} from './SaveAndSubmitForm';
+import { STORED_FROM_LAST_SESSION, SaveAndSubmitForm } from './SaveAndSubmitForm';
 
 const getInitialValue = (property: FormPropertyType) => {
   if (STORED_FROM_LAST_SESSION) {
@@ -29,10 +29,11 @@ const getInitialValue = (property: FormPropertyType) => {
 interface InputSectionProps {
   fieldNames: string[];
   properties: FormSectionType['properties'];
+  parentId?: string;
 }
 
 export const InputSection: React.FC<InputSectionProps> = props => {
-  const form = useForm({defaultValues: STORED_FROM_LAST_SESSION});
+  const form = useForm({ defaultValues: STORED_FROM_LAST_SESSION });
 
   React.useMemo(() => {
     // NOTE: remember those fields are unlikely to be changed even if we refetch and refetch...
@@ -40,7 +41,9 @@ export const InputSection: React.FC<InputSectionProps> = props => {
     // therefore we don't need to register again for every render
     props.fieldNames.forEach(fieldName => {
       const property = props.properties[fieldName];
-      form.register(fieldName, {
+      // Note: task doesn't mention that there are multi-nested forms rather mentions single so the fastest way for single nested in my own opinion is this
+      const formFieldName = props.parentId ? `${props.parentId}___${fieldName}` : fieldName;
+      form.register(formFieldName, {
         value: getInitialValue(property),
         required: true,
         pattern: property.regex
@@ -54,27 +57,29 @@ export const InputSection: React.FC<InputSectionProps> = props => {
     <FormProvider {...form}>
       {props.fieldNames.map(fieldName => {
         const property = props.properties[fieldName];
+        // Note: task doesn't mention that there are multi-nested forms rather mentions single so the fastest way for single nested in my own opinion is this
+        const formFieldName = props.parentId ? `${props.parentId}___${fieldName}` : fieldName;
         switch (property.type) {
           case 'text':
             return (
               <Controller
-                key={fieldName}
+                key={formFieldName}
                 control={form.control}
-                name={fieldName}
-                render={({field}) => (
+                name={formFieldName}
+                render={({ field }) => (
                   <React.Fragment>
                     <SingleLineTextInput
                       label={property.description}
                       value={field.value}
                       handleChange={field.onChange}
                       errorMessage={form.formState.errors[
-                        fieldName
+                        formFieldName
                       ]?.message?.toString()}
                       isDisabled={field.disabled}
                     />
 
                     <InputSubItems
-                      fieldName={fieldName}
+                      fieldName={formFieldName}
                       property={property}
                       values={field.value ? [field.value] : []}
                     />
@@ -85,24 +90,24 @@ export const InputSection: React.FC<InputSectionProps> = props => {
           case 'typeahead':
             return (
               <Controller
-                key={fieldName}
+                key={formFieldName}
                 control={form.control}
-                name={fieldName}
-                render={({field}) => (
+                name={formFieldName}
+                render={({ field }) => (
                   <React.Fragment>
                     <TypeAheadTextInput
                       label={property.description}
                       value={field.value}
                       handleChange={field.onChange}
                       errorMessage={form.formState.errors[
-                        fieldName
+                        formFieldName
                       ]?.message?.toString()}
                       isDisabled={field.disabled}
                       suggestions={property.enum!}
                     />
 
                     <InputSubItems
-                      fieldName={fieldName}
+                      fieldName={formFieldName}
                       property={property}
                       values={field.value ? [field.value] : []}
                     />
@@ -113,24 +118,24 @@ export const InputSection: React.FC<InputSectionProps> = props => {
           case 'select':
             return (
               <Controller
-                key={fieldName}
+                key={formFieldName}
                 control={form.control}
-                name={fieldName}
-                render={({field}) => (
+                name={formFieldName}
+                render={({ field }) => (
                   <React.Fragment>
                     <RadioGroupInput
                       label={property.description}
                       value={field.value}
                       handleChange={field.onChange}
                       errorMessage={form.formState.errors[
-                        fieldName
+                        formFieldName
                       ]?.message?.toString()}
                       isDisabled={field.disabled}
                       items={property.items!}
                     />
 
                     <InputSubItems
-                      fieldName={fieldName}
+                      fieldName={formFieldName}
                       property={property}
                       values={field.value ? [field.value] : []}
                     />
@@ -141,24 +146,24 @@ export const InputSection: React.FC<InputSectionProps> = props => {
           case 'multiselect':
             return (
               <Controller
-                key={fieldName}
+                key={formFieldName}
                 control={form.control}
-                name={fieldName}
-                render={({field}) => (
+                name={formFieldName}
+                render={({ field }) => (
                   <React.Fragment>
                     <CheckBoxGroupInput
                       label={property.description}
                       values={field.value}
                       handleChange={field.onChange}
                       errorMessage={form.formState.errors[
-                        fieldName
+                        formFieldName
                       ]?.message?.toString()}
                       isDisabled={field.disabled}
                       items={property.items!}
                     />
 
                     <InputSubItems
-                      fieldName={fieldName}
+                      fieldName={formFieldName}
                       property={property}
                       values={field.value ? [...field.value] : []}
                     />
@@ -203,6 +208,7 @@ export const InputSubItems: React.FC<InputSubItemsProps> = props => {
       <InputSection
         fieldNames={Object.keys(props.property.subitems || {})}
         properties={props.property.subitems || {}}
+        parentId={props.fieldName}
       />
     </Collapsible>
   );
